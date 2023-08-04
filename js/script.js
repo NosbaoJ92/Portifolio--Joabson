e01ec2b8e1fec1c9ff887cbb897952c49435a61c
NosbaoJ92 <joabson.nogueira@hotmail.com>
2023-08-04 09:27:58 -0300

    nova atualização v1.1

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function AbrirWEB() {
+    document.getElementById("projetos-web").style.display = "block";    
+    document.getElementById("projetos-mobile").style.display = "none";         
+    }
+function AbrirMOBILE() {
+    document.getElementById("projetos-mobile").style.display = "block";
+    document.getElementById("projetos-web").style.display = "none";   
+}
