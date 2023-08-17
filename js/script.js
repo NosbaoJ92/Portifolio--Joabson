f530e0ed3da0c8f461e0a541e8dcd78b14c43397
NosbaoJ92 <joabson.nogueira@hotmail.com>
2023-08-17 17:08:06 -0300

    novo commit

--- js/script.js
+++ js/script.js
@@ -1,19 +1,43 @@
 function AbrirWEB() {
     document.getElementById("projetos-web").style.display = "block";    
-    document.getElementById("projetos-mobile").style.display = "none";         
-    // document.getElementById("sobre").style.display = "none";         
+    document.getElementById("projetos-mobile").style.display = "none"; 
+    document.getElementById("closeMOBILE").style.display = "none"; 
+    document.getElementById("construcao").style.display = "none";
+    document.getElementById("closeMOBILE").style.display = "none";       
     }
+
+function FecharWEB() {
+    document.getElementById("artefato").style.display = "none";
+    document.getElementById("swift").style.display = "none";
+    document.getElementById("closeWEB").style.display = "none";
+}
+
 function AbrirMOBILE() {
     document.getElementById("projetos-mobile").style.display = "block";
     document.getElementById("projetos-web").style.display = "none"; 
-    // document.getElementById("sobre").style.display = "hidden"; 
+    document.getElementById("closeWEB").style.display = "none";
+    document.getElementById("artefato").style.display = "none";
+    document.getElementById("swift").style.display = "none";
+    document.getElementById("closeWEB").style.display = "none";
 }
+function FecharMOBILE() {
+    document.getElementById("construcao").style.display = "none";
+    document.getElementById("closeMOBILE").style.display = "none";
+}
+
 
 function AbrirArtefato() {
     document.getElementById("artefato").style.display = "block";
+    document.getElementById("closeWEB").style.display = "block";
     document.getElementById("swift").style.display = "none";
 }
+
 function AbrirSwift() {
     document.getElementById("swift").style.display = "block";
+    document.getElementById("closeWEB").style.display = "block";
     document.getElementById("artefato").style.display = "none";
+}
+function AbrirConstrucao() {
+    document.getElementById("construcao").style.display = "block";
+    document.getElementById("closeMOBILE").style.display = "block";
 }
