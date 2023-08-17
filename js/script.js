a19a043fcefc3bb3a916481a2f4591c1dff1a78b
NosbaoJ92 <joabson.nogueira@hotmail.com>
2023-08-17 11:50:24 -0300

    outro commit

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 function AbrirWEB() {
     document.getElementById("projetos-web").style.display = "block";    
     document.getElementById("projetos-mobile").style.display = "none";         
+    document.getElementById("sobre").style.display = "none";         
     }
 function AbrirMOBILE() {
     document.getElementById("projetos-mobile").style.display = "block";
-    document.getElementById("projetos-web").style.display = "none";   
+    document.getElementById("projetos-web").style.display = "none"; 
+    document.getElementById("sobre").style.display = "hidden"; 
 }
